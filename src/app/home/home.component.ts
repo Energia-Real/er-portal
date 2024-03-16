c93472a5bb13381d900633cee41677abcf15b3cc
edgar velasco <willsicko@gmail.com>
2024-03-16 17:49:18 -0600

    Highchart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,12 +5,22 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { LayoutModule } from '@app/_shared/components/layout/layout.module';
 import { SelectionModel } from '@angular/cdk/collections';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { HighchartsChartModule } from 'highcharts-angular';
+import * as Highcharts from 'highcharts';
 
 const materialModules = [
   MatButtonModule,
   MatIconModule,
   MatTableModule,
-  MatCheckboxModule
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatDatepickerModule,
+  MatSelectModule,
+  HighchartsChartModule
 ];
 
 export interface PeriodicElement {
@@ -21,16 +31,16 @@ export interface PeriodicElement {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
 ];
 
 @Component({
@@ -38,12 +48,59 @@ const ELEMENT_DATA: PeriodicElement[] = [
   standalone: true,
   templateUrl: './home.component.html',
   imports: [LayoutModule, ...materialModules],
-  styleUrl: './home.component.sass'
+  styleUrl: './home.component.scss',
+  providers: [provideNativeDateAdapter()],
 })
 export class HomeComponent {
   displayedColumns: string[] = ['select', 'position', 'name', 'weight', 'symbol'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
   selection = new SelectionModel<PeriodicElement>(true, []);
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    chart: {
+      type: 'column'
+    },
+    colors: ['#d9d9d9', '#ee5427', '#57b1b1', '#792430', '#000000'],
+    title: {
+      text: '',
+      align: 'left'
+    },
+    xAxis: {
+      categories: ['Arsenal', 'Chelsea', 'Liverpool', 'Manchester United']
+    },
+    yAxis: {
+      allowDecimals: false,
+      min: 0,
+      title: {
+        text: ''
+      }
+    },
+    tooltip: {
+      format: '<b>{key}</b><br/>{series.name}: {y}<br/>' +
+        'Total: {point.stackTotal}'
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal'
+      }
+    },
+    series: [{
+      name: 'ICO2',
+      data: [3, 5, 1, 13]
+    }, {
+      name: 'Subtotal ER',
+      data: [14, 8, 8, 12]
+    }, {
+      name: 'Saving (MXN)',
+      data: [0, 2, 6, 3]
+    }, {
+      name: 'Subtotal CFE',
+      data: [17, 2, 6, 3]
+    }, {
+      name: 'CFE Cost w/o Solar',
+      data: [0, 2, 6, 3]
+    }] as any
+  };
 
   /** Whether the number of selected elements matches the total number of rows. */
   isAllSelected() {
