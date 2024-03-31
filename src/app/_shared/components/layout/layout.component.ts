0f6b6ba6eae76dde7b077f367a911b7dac099a74
edgar velasco <willsicko@gmail.com>
2024-03-31 13:41:40 -0600

    Auto stash before checking out "origin/main"

--- src/app/_shared/components/layout/layout.component.ts
+++ src/app/_shared/components/layout/layout.component.ts
@@ -3,6 +3,7 @@ import { Location } from '@angular/common';
 
 import { AccountService } from '@app/_services/account.service';
 import { User } from '@app/_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
@@ -13,14 +14,15 @@ export class LayoutComponent {
   userInfo: User | null = null;
   @Input() routeActive = '';
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private router: Router) {}
 
   ngOnInit(): void {
     this.getInfoUser();
   }
 
   signOut() {
-    window.location.href = '/';
+    localStorage.removeItem('user');
+    this.router.navigate(['/account/login']);
   }
 
   getInfoUser() {
