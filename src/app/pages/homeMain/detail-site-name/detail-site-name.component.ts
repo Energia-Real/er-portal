18a616db2680b94f86f1d938a05814a8ac0c73d7
Jose Carlos LB <joslos_carlos@hotmail.com>
2024-04-11 18:35:01 -0600

    Modulo para ver la información del cliente

--- src/app/pages/homeMain/detail-site-name/detail-site-name.component.ts
+++ src/app/pages/homeMain/detail-site-name/detail-site-name.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { LayoutModule } from '@app/shared/components/layout/layout.module';
 import { MaterialModule } from '@app/shared/material/material.module';
+import * as Highcharts from 'highcharts';
 
 @Component({
   selector: 'app-detail-site-name',
@@ -14,5 +15,54 @@ import { MaterialModule } from '@app/shared/material/material.module';
 
 })
 export class DetailSiteNameComponent {
-
+  Highcharts: typeof Highcharts = Highcharts;
+  chartOptions: Highcharts.Options = {
+    chart: {
+      type: 'column'
+    },
+    colors: ['#d9d9d9', '#ee5427', '#57b1b1', '#792430', '#000000'],
+    title: {
+      text: '',
+      align: 'left'
+    },
+    xAxis: {
+      categories: []
+      // categories: ['Arsenal', 'Chelsea', 'Liverpool', 'Manchester United']
+    },
+    yAxis: {
+      allowDecimals: false,
+      min: 0,
+      title: {
+        text: ''
+      }
+    },
+    tooltip: {
+      format: ''
+      // format: '<b>{key}</b><br/>{series.name}: {y}<br/>' +
+      //   'Total: {point.stackTotal}'
+    },
+    plotOptions: {
+      column: {
+        stacking: 'normal'
+      }
+    },
+    series: [
+      {
+      name: '',
+      data: [3, 5, 1, 13]
+    }, {
+      name: '',
+      data: [14, 8, 8, 12]
+    }, {
+      name: '',
+      data: [0, 2, 6, 3]
+    }, {
+      name: '',
+      data: [17, 2, 6, 3]
+    }, {
+      name: '',
+      data: [0, 2, 6, 3]
+    }
+  ] as any
+  };
 }
