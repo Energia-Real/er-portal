6f8e38429b9ad90e202a0ac6325aaeaa24ab5529
Mario Enrique Chan Fernandez <mario@energiareal.mx>
2024-05-06 17:33:13 -0600

    "er-backoffice-root-mf-mx" to "er-portal":

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, er-backoffice-root-mf-mx');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, er-portal');
   });
 });
