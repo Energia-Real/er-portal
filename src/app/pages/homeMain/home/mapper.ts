c41190565b495499814eb436510ea0b6e3a43d32
Jose Carlos LB <joslos_carlos@hotmail.com>
2024-05-27 16:06:57 -0600

    Servicio listo

--- src/app/pages/homeMain/home/mapper.ts
+++ src/app/pages/homeMain/home/mapper.ts
@@ -1,9 +1,10 @@
+import { FormatsService } from '@app/shared/services/formats.service';
 import * as entity from './home-model';
 
 export class Mapper {
-	static getDataClientsMapper(response: entity.DataRespSavingDetails[]) : entity.DataRespSavingDetailsMapper {
-		let totalEnergyConsumption:number = 0;
-		let totalEnergyProduction:number = 0;
+	static getDataClientsMapper(response: entity.DataRespSavingDetails[], formatsService: FormatsService ): entity.DataRespSavingDetailsMapper {
+		let totalEnergyConsumption: number = 0;
+		let totalEnergyProduction: number = 0;
 
 		let dataList: entity.DataRespSavingDetails[] = [];
 
@@ -14,23 +15,23 @@ export class Mapper {
 			totalEnergyProduction += energyProduction;
 
 			dataList.push({
-				siteId : data?.siteId || '-',
-				siteName : data?.siteName || '-',
-				siteSaving : data?.siteSaving,
-				cfeZone : data?.cfeZone,
-				solarCoverage : data?.solarCoverage,
-				co2Saving : data?.co2Saving,
-				energyConsumption : data?.energyConsumption || '-',
-				energyProduction : data?.energyProduction || '-'
-			})
+				siteId: data?.siteId || '-',
+				siteName: data?.siteName || '-',
+				siteSaving: data?.siteSaving,
+				cfeZone: data?.cfeZone,
+				solarCoverage: data?.solarCoverage,
+				co2Saving: data?.co2Saving,
+				energyConsumption: formatsService.energyFormat(parseFloat(data?.energyConsumption)),
+				energyProduction: formatsService.energyFormat(parseFloat(data?.energyProduction)),
+			});
 		});
 
 		return {
-			data : dataList,
-			savingDetails : {
-				totalEnergyConsumption,
-				totalEnergyProduction,
+			data: dataList,
+			savingDetails: {
+				totalEnergyConsumption: formatsService.energyFormat(totalEnergyConsumption),
+				totalEnergyProduction: formatsService.energyFormat(totalEnergyProduction),
 			}
-		}
+		};
 	}
 }
