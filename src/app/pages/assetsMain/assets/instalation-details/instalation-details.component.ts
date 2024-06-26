1e45bd7aa1b4d3e28a11b9720ea91aad8c84f802
TorrecillaER <aaron@energiareal.mx>
2024-06-26 10:07:47 -0600

    feat: pdf in instalation details

--- src/app/pages/assetsMain/assets/instalation-details/instalation-details.component.ts
+++ src/app/pages/assetsMain/assets/instalation-details/instalation-details.component.ts
@@ -1,70 +1,39 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import * as entity from '../assets-model';
 import Highcharts from 'highcharts';
+import { PdfViewerComponent as Ng2PdfViewerComponent } from 'ng2-pdf-viewer';
 
 @Component({
   selector: 'app-instalation-details',
   templateUrl: './instalation-details.component.html',
   styleUrl: './instalation-details.component.scss'
 })
-export class InstalationDetailsComponent implements OnInit{
+export class InstalationDetailsComponent implements OnInit, AfterViewInit{
   @Input() assetData!: entity.DataPlant;
   @Input() notData! : boolean;
-
+  @ViewChild(Ng2PdfViewerComponent) private pdfViewer!: Ng2PdfViewerComponent;
+  @ViewChild('canvas') canvasRef!: ElementRef<HTMLCanvasElement>;
+  pdfSrc = '/assets/pdfs/pdf_prueba.pdf';
   showAlert: boolean = false
   Highcharts: typeof Highcharts = Highcharts;
+  images: string[] = [];
+  renderedImage: string | null = null;
 
-  chartOptions: Highcharts.Options = {
-    chart: {
-      type: 'column'
-    },
-    colors: ['#d9d9d9', '#ee5427', '#57b1b1', '#792430', '#000000'],
-    title: {
-      text: '',
-      align: 'left'
-    },
-    xAxis: {
-      categories: []
-    },
-    yAxis: {
-      allowDecimals: false,
-      min: 0,
-      title: {
-        text: ''
-      }
-    },
-    tooltip: {
-      format: ''
-      // format: '<b>{key}</b><br/>{series.name}: {y}<br/>' +
-      //   'Total: {point.stackTotal}'
-    },
-    plotOptions: {
-      column: {
-        stacking: 'normal'
-      }
-    },
-    series: [
-      {
-        name: '',
-        data: [3, 5, 1, 13]
-      }, {
-        name: '',
-        data: [14, 8, 8, 12]
-      }, {
-        name: '',
-        data: [0, 2, 6, 3]
-      }, {
-        name: '',
-        data: [17, 2, 6, 3]
-      }, {
-        name: '',
-        data: [0, 2, 6, 3]
-      }
-    ] as any
-  };
 
   
   ngOnInit(): void {
     if (this.notData) this.showAlert = true;
   }
+
+  ngAfterViewInit(): void {
+    
+  }
+
+  onPageRendered(event: CustomEvent): void {
+    const canvas: HTMLCanvasElement = event.target as HTMLCanvasElement;
+    this.renderedImage = canvas.toDataURL('image/png');
+  }
+
+
+  
 }
