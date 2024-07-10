e1bbe3ca2060adca9bb727be47f82e7df1601135
Jose Carlos LB <joslos_carlos@hotmail.com>
2024-07-10 13:33:45 -0600

    feat: avances

--- src/app/pages/paymets-main/select-pay/select-pay.component.ts
+++ src/app/pages/paymets-main/select-pay/select-pay.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormatsService } from '@app/shared/services/formats.service';
+import { OpenModalsService } from '@app/shared/services/openModals.service';
 
 @Component({
   selector: 'app-select-pay',
@@ -6,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './select-pay.component.scss'
 })
 export class SelectPayComponent {
+
+  
+  formFilters = this.formBuilder.group({
+    rangeDateStart: [{ value: '', disabled: false }],
+    rangeDateEnd: [{ value: '', disabled: false }],
+  });
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private notificationService: OpenModalsService,
+    private formatsService: FormatsService
+  ) { }
 
 }
