12668a8cbd0c32a46cfedd2382da361a1d82b266
Jose Carlos LB <joslos_carlos@hotmail.com>
2024-10-08 11:21:12 -0600

    feat: avance

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,8 +1,8 @@
-import { Component, EventEmitter, Input, OnDestroy, Output, ViewChild } from '@angular/core';
+import { Component, EventEmitter, Input, NgZone, OnDestroy, Output, ViewChild } from '@angular/core';
 
 import { Router } from '@angular/router';
 import { AuthService } from '@app/auth/auth.service';
-import { Subject, takeUntil } from 'rxjs';
+import { Subject, take, takeUntil } from 'rxjs';
 import packageJson from '../../../../../package.json';
 import { FormBuilder } from '@angular/forms';
 import { setFilters, setFiltersBatu, setFiltersSolarCoverage } from '@app/core/store/actions/filters.actions';
@@ -63,32 +63,60 @@ export class HeaderComponent implements OnDestroy {
 
   selectedMonths: any[] = []
 
+  currentFilters: any = {};
+  currentFiltersBatu: any = {};
+  currentFiltersSolarCoverage: any = {};
+
   constructor(
     private accountService: AuthService,
     private router: Router,
+    private zone: NgZone,
     private store: Store<{ filters: FilterState }>
   ) { }
 
+  // ngOnInit(): void {
+  //   this.store.select(selectFilters).pipe(takeUntil(this.onDestroy$)).subscribe(filtersState => {
+  //     console.log('SE TRAJERON', filtersState);
+
+  //     if (filtersState && filtersState?.months?.length) {
+  //       this.selectedMonths = this.months.filter(month => {
+  //         const filterMonth = `${this.currentYear}-${month.value}-01`;
+  //         return filtersState.months.includes(filterMonth);
+  //       });
+
+  //       if (this.selectedMonths?.length) {
+  //         this.selectedStartMonth = this.selectedMonths[0];
+  //         this.selectedEndMonth = this.selectedMonths[this.selectedMonths.length - 1];
+  //       } else {
+  //         this.setDefaultMonths();
+  //       }
+  //     } else this.setDefaultMonths();
+  //   });
+
+
+  //   this.getInfoUser();
+  // }
+
   ngOnInit(): void {
-    this.store.select(selectFilters).pipe(takeUntil(this.onDestroy$)).subscribe(filtersState => {
+    this.store.select(selectFilters).pipe(takeUntil(this.onDestroy$)).subscribe((filtersState: any) => {
       if (filtersState && filtersState.months.length > 0) {
-        this.selectedMonths = this.months.filter(month =>
-          filtersState.months.includes(`${this.currentYear}-${month.value}-01`)
-        );
-        this.selectedStartMonth = this.selectedMonths[0];
-        this.selectedEndMonth = this.selectedMonths[this.selectedMonths.length - 1];
+        const formattedSelectedMonths = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}-01`);
+        const isSameFilters = JSON.stringify(filtersState?.months) === JSON.stringify(formattedSelectedMonths);
+  
+        if (!isSameFilters) {
+          // Solo actualizar si los filtros no son los mismos
+          this.updateSelectedMonths();
+        }
       } else {
         this.setDefaultMonths();
       }
     });
-
-    this.getInfoUser();
   }
-
+  
   setDefaultMonths() {
     this.selectedStartMonth = this.months[5];
     this.selectedEndMonth = this.months[6];
-    // this.updateSelectedMonths();
+    this.updateSelectedMonths();
   }
 
   selectStartMonth(month: any, menuTrigger: MatMenuTrigger): void {
@@ -109,19 +137,16 @@ export class HeaderComponent implements OnDestroy {
     if (this.selectedStartMonth && this.selectedEndMonth) {
       const startIndex = this.months.findIndex(m => m.value === this.selectedStartMonth.value);
       const endIndex = this.months.findIndex(m => m.value === this.selectedEndMonth.value);
-
       if (startIndex !== -1 && endIndex !== -1) {
         const selectedRange = this.months.slice(
           Math.min(startIndex, endIndex),
           Math.max(startIndex, endIndex) + 1
         );
-
-        this.selectedMonths = selectedRange;
-        console.log(`Rango de meses seleccionado: ${this.selectedMonths.map(m => m.name).join(', ')}`);
+        this.selectedMonths = [...selectedRange];
       }
     }
-
     this.searchWithFilters();
+
   }
 
   searchWithFilters() {
@@ -131,24 +156,78 @@ export class HeaderComponent implements OnDestroy {
       brand: "huawei",
       clientName: "Merco",
       months: []
-    }
-
-    if (this.selectedMonths.length) {
+    };
+  
+    if (this.selectedMonths?.length) {
+      const formattedSelectedMonths = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}-01`);
+  
       filters.requestType = 'Month';
-      filters.months = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}-01`);
-
+      filters.months = formattedSelectedMonths;
+  
       filtersSolarCoverage.requestType = 2;
-      filtersSolarCoverage.months = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}-01`);
+      filtersSolarCoverage.months = formattedSelectedMonths;
       filtersBatu.months = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}`);
-
+  
+      // Despachar directamente sin verificar los filtros en esta función
       this.store.dispatch(setFilters({ filters }));
       this.store.dispatch(setFiltersBatu({ filtersBatu }));
       this.store.dispatch(setFiltersSolarCoverage({ filtersSolarCoverage }));
-      console.log('filters', filters);
-      console.log('filtersBatu', filtersBatu);
-      console.log('filtersSolarCoverage', filtersSolarCoverage);
+  
+      console.log('Nuevos filtros despachados:', filters);
+      console.log('Nuevos filtrosBatu despachados:', filtersBatu);
+      console.log('Nuevos filtrosSolarCoverage despachados:', filtersSolarCoverage);
     }
   }
+
+  
+  // searchWithFilters() {
+  //   let filtersBatu: any = {};
+  //   let filters: any = {};
+  //   let filtersSolarCoverage: any = {
+  //     brand: "huawei",
+  //     clientName: "Merco",
+  //     months: []
+  //   }
+
+  //   if (this.selectedMonths?.length) {
+  //     const formattedSelectedMonths = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}-01`);
+  //     console.log('formattedSelectedMonths', formattedSelectedMonths);
+      
+
+  //     filters.requestType = 'Month';
+  //     filters.months = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}-01`);
+
+  //     filtersSolarCoverage.requestType = 2;
+  //     filtersSolarCoverage.months = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}-01`);
+  //     filtersBatu.months = this.selectedMonths.map(month => `${this.currentYearComplete}-${month.value}`);
+
+
+  //     this.store.select(selectFilters).pipe(takeUntil(this.onDestroy$)).subscribe((currentFilters: any) => {
+  //       console.log('currentFilters', currentFilters);
+
+  //       if (currentFilters && currentFilters?.months?.length) {
+  //         const isSameFilters = JSON.stringify(currentFilters?.months) === JSON.stringify(formattedSelectedMonths);
+  //         console.log('isSameFilters', isSameFilters);
+
+  //         if (!isSameFilters) {
+  //           this.store.dispatch(setFilters({ filters }));
+  //           this.store.dispatch(setFiltersBatu({ filtersBatu }));
+  //           this.store.dispatch(setFiltersSolarCoverage({ filtersSolarCoverage }));
+  //         }
+  //       }
+
+  //       else {
+  //         this.store.dispatch(setFilters({ filters }));
+  //         this.store.dispatch(setFiltersBatu({ filtersBatu }));
+  //         this.store.dispatch(setFiltersSolarCoverage({ filtersSolarCoverage }));
+  //       }
+  //     });
+
+  //     console.log('filters', filters);
+  //     console.log('filtersBatu', filtersBatu);
+  //     console.log('filtersSolarCoverage', filtersSolarCoverage);
+  //   }
+  // }
 
   signOut() {
     localStorage.removeItem('userEnergiaReal');
