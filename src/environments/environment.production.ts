dfb0b3dd385d9173b922fe4aeb717ada58304e77
TorrecillaER <aaron@energiareal.mx>
2025-01-06 11:08:35 -0600

    Update environment.production.ts

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -10,6 +10,7 @@ export const environment = {
   API_URL_EQUIPMENT_HUAWEI_V1: "https://er-equipment-monitoring-ms.azurewebsites.net/api/huawei/v1",
   API_URL_ENERGY_PERFORMANCE_V1: "https://er-projects-energy-performance.azurewebsites.net/api/v1",
   API_URL_BILL_V1: "https://er-projects-bill.azurewebsites.net/api",
+  API_URL_NOTIFICATIONS: "https://er-notifications.azurewebsites.net/api/v1",
   APP: 'backoffice',
   GOOGLE_API_KEY: 'AIzaSyAO4bqxxS4-CkLR_vY1RKJNwDanh5vyvq8',
   TOMORROW_KEY: '8lD2yWTpkIuHYCXCXNtBhMkeghYU4dau',
