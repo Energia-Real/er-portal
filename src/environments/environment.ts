1f326f06dad56d0448177492460eb18f59114fad
Mario Enrique Chan Fernandez <mario@energiareal.mx>
2025-01-30 17:26:30 -0600

    fix: corregir rutas

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: false,
-  API_URL_PERFORMANCE: 'https://er-performance-api.azurewebsites.net/api/v1',
-  API_URL_AUTH_V1: 'https://er-backoffice-auth-ms-mx.azurewebsites.net/api/v1',
+  API_URL_PERFORMANCE: 'https://er-portal-gateway.azurewebsites.net/dev/api/v1',
+  API_URL_AUTH_V1: 'https://er-auth-app.azurewebsites.net/api/v1',
   APP: 'backoffice',
   GOOGLE_API_KEY: 'AIzaSyAO4bqxxS4-CkLR_vY1RKJNwDanh5vyvq8',
   TOMORROW_KEY: '8lD2yWTpkIuHYCXCXNtBhMkeghYU4dau',
-  CRYPTO_KEY: '4Vx!9H7j@LkP*2qZ$m#F&tWdRp^C!8s'
+  CRYPTO_KEY: '4Vx!9H7j@LkP*2qZ$m#F&tWdRp^C!8s',
 };
