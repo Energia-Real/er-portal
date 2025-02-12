2c25bd3d95cb090a090f56295f89acca97814c25
JoseCarloslbER <165943797+JoseCarloslbER@users.noreply.github.com>
2025-02-12 13:32:31 -0600

    feat: estructura plant details

---
+++ src/app/pages/plants-main/plant-details/inverter/inverter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inverter',
+  templateUrl: './inverter.component.html',
+  styleUrl: './inverter.component.scss'
+})
+export class InverterComponent {
+
+}
