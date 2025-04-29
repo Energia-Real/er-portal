cc90deb8772732579263f4843f0b4f434e7121fd
Mario Enrique Chan Fernandez <mario@energiareal.mx>
2025-04-29 17:56:23 -0600

    fix: nuevo server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,12 @@
 export const environment = {
   production: false,
+  API_URL_DOMAIN_BACKEND: 'er-domain-backend.azurewebsites.net/api',
   API_URL_PERFORMANCE: 'https://er-portal-gateway.azurewebsites.net/dev/api/v1',
   API_URL_AUTH_V1: 'https://er-auth-app.azurewebsites.net/api/v1/realm/dev',
-  API_URL_FINANTIAL_MODEL: 'https://er-financial-model-calc-ms.azurewebsites.net/api/',
-  API_URL_FINANTIAL_MODEL_SOCKET: 'wss://er-financial-model-calc-ms.azurewebsites.net/api/',
+  API_URL_FINANTIAL_MODEL:
+    'https://er-financial-model-calc-ms.azurewebsites.net/api/',
+  API_URL_FINANTIAL_MODEL_SOCKET:
+    'wss://er-financial-model-calc-ms.azurewebsites.net/api/',
   APP: 'backoffice',
   GOOGLE_API_KEY: 'AIzaSyAO4bqxxS4-CkLR_vY1RKJNwDanh5vyvq8',
   TOMORROW_KEY: '8lD2yWTpkIuHYCXCXNtBhMkeghYU4dau',
