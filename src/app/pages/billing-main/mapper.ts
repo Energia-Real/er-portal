63c95c8443437c1dbd2511d41d0df2d90442b9c0
JoseCarloslbER <165943797+JoseCarloslbER@users.noreply.github.com>
2025-05-12 13:49:05 -0700

    feat: grafica de overview mejroada

--- src/app/pages/billing-main/mapper.ts
+++ src/app/pages/billing-main/mapper.ts
@@ -5,7 +5,7 @@ export class Mapper {
 	static getBillingDataMapper(response: entity.DataBillingTableMapper, formatsService: FormatsService): entity.DataBillingTableMapper {
 		let dataList: entity.DataBillingTable[] = [];
 		console.log(response.data);
-		
+
 
 		response?.data?.forEach((data: entity.DataBillingTable): void => {
 			dataList.push({
@@ -25,7 +25,7 @@ export class Mapper {
 			data: dataList
 		}
 	}
-	
+
 	static getBillingOverviewMapper(response: entity.DataBillingOverviewTableMapper, formatsService: FormatsService): entity.DataBillingOverviewTableMapper {
 		let dataList: entity.DataBillingOverviewTable[] = [];
 
@@ -75,67 +75,65 @@ export class Mapper {
 
 		return {
 			...response,
-			dataPlants : dataList
+			dataPlants: dataList
 		}
 	}
 
 	static getEnergysummaryMapper(response: entity.EnergyBillingSummary, formatsService: FormatsService): ChartConfiguration<'bar' | 'line'>['data'] | any {
-	  
+
 		console.log(response.response);
-		
+
 		const monthsMap = [
-		  'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-		  'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
+			'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+			'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
 		];
-	  
+
 		const fullMonths = Array.from({ length: 12 }, (_, i) => {
-		  const found = response.response.energySummaryResponse.months.find(m => m.month === i + 1);
-		  return {
-			label: `${monthsMap[i]} 25`,
-			billedEnergyProduced: found ? found.billedEnergyProduced : 0,
-			billedEnergy: found ? found.billedEnergy : 0,
-			billedEnergyAmouth: found ? found.billedEnergyAmouth : 0,
-		  };
+			const found = response.response.energySummaryResponse.months.find(m => m.month === i + 1);
+			return {
+				label: `${monthsMap[i]} 25`,
+				billedEnergyProduced: found ? found.billedEnergyProduced : 0,
+				billedEnergy: found ? found.billedEnergy : 0,
+				billedEnergyAmouth: found ? found.billedEnergyAmouth : 0,
+			};
 		});
-	  
+
 		return {
-		  labels: fullMonths.map(m => m.label),
-		  datasets: [
-			{
-			  type: 'bar',
-			  label: 'Energy Produced (kWh)',
-			  data: fullMonths.map(m => +m.billedEnergyProduced.toFixed(2)),
-			  backgroundColor: '#F97316',
-			  barPercentage: 0.8,
-			  categoryPercentage: 0.9,
-			  order: 1,
-			},
-			{
-			  type: 'bar',
-			  label: 'Energy Billed (kWh)',
-			  data: fullMonths.map(m => +m.billedEnergy.toFixed(2)),
-			  backgroundColor: '#57B1B1',
-			  barPercentage: 0.8,
-			  categoryPercentage: 0.9,
-			  order: 1,
-			},
-			{
-			  type: 'line',
-			  label: 'Total Amount',
-			  data: fullMonths.map(m => +m.billedEnergyAmouth.toFixed(2)),
-			  borderColor: '#6B021A',
-			  backgroundColor: '#6B021A',
-			  tension: 0.4,
-			  pointRadius: 4,
-			  pointBackgroundColor: '#6B021A',
-			  borderWidth: 2,
-			  order: 3,
-			}
-		  ],
-		  balance: formatsService.moneyFormat(response.response.energySummaryResponse.balance),
+			labels: fullMonths.map(m => m.label),
+			datasets: [
+				{
+					type: 'bar',
+					label: 'Energy Produced (kWh)',
+					data: fullMonths.map(m => +m.billedEnergyProduced.toFixed(2)),
+					backgroundColor: '#F97316',
+					barPercentage: 0.8,
+					categoryPercentage: 0.9,
+					order: 1,
+				},
+				{
+					type: 'bar',
+					label: 'Energy Billed (kWh)',
+					data: fullMonths.map(m => +m.billedEnergy.toFixed(2)),
+					backgroundColor: '#57B1B1',
+					barPercentage: 0.8,
+					categoryPercentage: 0.9,
+					order: 1,
+				},
+				{
+					type: 'line',
+					data: fullMonths.map(m => +m.billedEnergyAmouth.toFixed(2)),
+					borderColor: '#6B021A',
+					backgroundColor: '#6B021A',
+					pointBackgroundColor: '#6B021A',
+					pointBorderColor: '#6B021A',
+					label: 'Total Amount',
+					order: 0,
+				}
+			],
+			balance: formatsService.moneyFormat(response.response.energySummaryResponse.balance),
 		};
 
-	  }
-	  
+	}
+
 
 }
