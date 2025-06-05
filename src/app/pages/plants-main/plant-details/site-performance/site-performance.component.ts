e32d933850bb0b973eb2debb7010176cfe2df5c2
José Carlos LB <165943797+JoseCarloslbER@users.noreply.github.com>
2025-06-05 15:56:22 -0700

    feat: avance

--- src/app/pages/plants-main/plant-details/site-performance/site-performance.component.ts
+++ src/app/pages/plants-main/plant-details/site-performance/site-performance.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subject, takeUntil } from 'rxjs';
+import { forkJoin, Observable, Subject, takeUntil } from 'rxjs';
 import * as entity from '../../plants-model';
 import { FormBuilder } from '@angular/forms';
 import { Chart, ChartConfiguration, ChartOptions } from "chart.js";
@@ -30,7 +30,6 @@ export class SitePerformanceComponent implements OnInit, OnDestroy {
 
   chart: any;
   lineChartData!: ChartConfiguration<'bar' | 'line'>['data'];
-
   lineChartOptions: ChartOptions<'bar' | 'line'> = {
     responsive: true,
     maintainAspectRatio: false,
@@ -128,6 +127,9 @@ export class SitePerformanceComponent implements OnInit, OnDestroy {
 
   isLoading: boolean = true;
 
+  labelsChart: { key: string; text: string; }[] = [];
+  chartDataCache: any = null;
+
   constructor(
     private formBuilder: FormBuilder,
     private moduleServices: PlantsService,
@@ -147,76 +149,92 @@ export class SitePerformanceComponent implements OnInit, OnDestroy {
     this.getStatus();
     this.getUserClient();
 
-    // Subscribe to language changes
     this.translationService.currentLang$
       .pipe(takeUntil(this.onDestroy$))
-      .subscribe(() => this.getUserClient());
+      .subscribe(() => {
+        this.initializeTranslations();
+      });
   }
 
-  getUserClient() {
-    const encryptedData = localStorage.getItem('userInfo');
-    if (encryptedData) {
-      const userInfo = this.encryptionService.decryptData(encryptedData);
-      this.generalFilters$.subscribe((generalFilters: GeneralFilters) => {
-        this.getSitePerformance({ clientId: userInfo.clientes[0], ...generalFilters });
-      });
-    }
+  initializeTranslations(): void {
+    const keysChart2 = [
+      'DETALLE_PLANTA.GENERACION',
+      'DETALLE_PLANTA.CONSUMO_CFE_RED',
+      'DETALLE_PLANTA.CONSUMO_CFE'
+    ];
+
+    const translations2$ = forkJoin(keysChart2.map(k => this.translationService.getTranslation(k)));
+
+    forkJoin([translations2$]).subscribe(([[generacion, produccion, consumo]]) => {
+      this.labelsChart = [
+        { key: keysChart2[0], text: generacion },
+        { key: keysChart2[1], text: produccion },
+        { key: keysChart2[2], text: consumo }
+      ];
+
+      this.updateClientsChart();
+    });
+  }
+
+
+  updateClientsChart() {
+    const dataResponse = this.chartDataCache;
+    if (!dataResponse) return;
+
+    const labels = this.labelsChart.map(l => l.text);
+    this.sitePerformance.primaryElements = dataResponse.primaryElements;
+    this.sitePerformance.additionalItems = dataResponse.additionalItems;
+    const cfeConsumption = dataResponse.monthlyData?.map((item: any) => item.cfeConsumption ?? 0);
+    const consumption = dataResponse.monthlyData?.map((item: any) => item.consumption ?? 0);
+    const generation = dataResponse.monthlyData?.map((item: any) => item.generation ?? 0);
+    const exportedSolarGeneration = dataResponse.monthlyData?.map((item: any) => item.exportedGeneration ?? 0);
+
+    console.log('dataResponse.monthlyData', dataResponse.monthlyData);
+    
+    this.lineChartData = {
+      labels: dataResponse.monthlyData?.map((item: any) => {
+        return item.month;
+      }),
+      datasets: [
+        {
+          type: 'bar',
+          data: cfeConsumption ?? [],
+          label: labels[0],
+          backgroundColor: 'rgba(121, 36, 48, 1)',
+          order: 1
+        },
+        {
+          type: 'bar',
+          data: generation ?? [],
+          label: labels[1],
+          backgroundColor: 'rgba(87, 177, 177, 1)',
+          order: 1
+        },
+        {
+          type: 'line',
+          data: consumption ?? [],
+          label: labels[2],
+          borderColor: 'rgba(239, 68, 68, 1)',
+          backgroundColor: 'rgba(239, 68, 68, 1)',
+          pointBackgroundColor: 'rgba(239, 68, 68, 1)',
+          pointBorderColor: 'rgba(239, 68, 68, 1)',
+          order: 0,
+        }
+      ]
+    };
+
+    this.displayChart = true;
+    this.isLoading = false;
+    this.initChart();
+    this.chart?.update();
   }
 
   getSitePerformance(filters: GeneralFilters) {
     this.moduleServices.getSitePerformanceDetails(this.plantData.id, filters).subscribe({
       next: (response: entity.DataResponseArraysMapper | null) => {
         if (response) {
-          this.sitePerformance.primaryElements = response.primaryElements;
-          this.sitePerformance.additionalItems = response.additionalItems;
-          // this.fullLoad = true;
-          const cfeConsumption = response.monthlyData?.map(item => item.cfeConsumption ?? 0);
-          const consumption = response.monthlyData?.map(item => item.consumption ?? 0);
-          const generation = response.monthlyData?.map(item => item.generation ?? 0);
-          const exportedSolarGeneration = response.monthlyData?.map(item => item.exportedGeneration ?? 0);
-
-          this.lineChartData = {
-            labels: response.monthlyData?.map((item) => {
-              return item.month;
-            }),
-            datasets: [
-              {
-                type: 'bar',
-                data: cfeConsumption ?? [],
-                label: 'CFE network consumption (MWh)',
-                backgroundColor: 'rgba(121, 36, 48, 1)',
-                order: 1
-              },
-              /*  {
-                 type: 'bar',
-                 data: exportedSolarGeneration ?? [],
-                 label: 'Exported solar generation (MWh)',
-                 backgroundColor: 'rgba(255, 71, 19, 1)',
-                 order: 1
-               }, */
-              {
-                type: 'bar',
-                data: generation ?? [],
-                label: 'Generation (MWh)',
-                backgroundColor: 'rgba(87, 177, 177, 1)',
-                order: 1
-              },
-              {
-                type: 'line',
-                data: consumption ?? [],
-                borderColor: 'rgba(239, 68, 68, 1)',
-                backgroundColor: 'rgba(239, 68, 68, 1)',
-                pointBackgroundColor: 'rgba(239, 68, 68, 1)',
-                pointBorderColor: 'rgba(239, 68, 68, 1)',
-                label: 'Consumption (MWh)',
-                order: 0,
-              }
-            ]
-          };
-
-          this.displayChart = true;
-          this.isLoading = false;
-          this.initChart();
+          this.chartDataCache = response;
+          this.updateClientsChart()
         }
       },
       error: (error) => {
@@ -237,6 +255,16 @@ export class SitePerformanceComponent implements OnInit, OnDestroy {
         console.error(error)
       }
     })
+  }
+
+  getUserClient() {
+    const encryptedData = localStorage.getItem('userInfo');
+    if (encryptedData) {
+      const userInfo = this.encryptionService.decryptData(encryptedData);
+      this.generalFilters$.subscribe((generalFilters: GeneralFilters) => {
+        this.getSitePerformance({ clientId: userInfo.clientes[0], ...generalFilters });
+      });
+    }
   }
 
   refreshChart(index?: number): void {
